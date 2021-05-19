0860a840923d4502261c1746c29ed80f2cd437e0
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-05-19 21:44:35 +0530

    footer working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
 import "./Navbar.css";
@@ -23,14 +23,22 @@ function Navbar() {
         }
     };
 
+    useEffect(() => {
+        showButton();
+    }, [button]);
+
     window.addEventListener("resize", showButton);
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        TRVL <i className="fab fa-typo3"></i>
+                    <Link
+                        to="/"
+                        className="navbar-logo"
+                        onClick={closeMobileMenu}
+                    >
+                        VOYAGE <i className="fas fa-ship"></i>
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? "fas fa-times" : "fas fa-bars"} />
